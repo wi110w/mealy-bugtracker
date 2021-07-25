b244e658ae33566aa41195b425c2cf9b96ed6ead
Eugenia Glinska <wi110w@nyan.org.ua>
2021-07-25 19:09:43 +0300

    Rename swagger title

--- Mealy BT/Startup.cs
+++ Mealy BT/Startup.cs
@@ -27,7 +27,7 @@ namespace Mealy_BT
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Mealy_BT", Version = "v1"}); });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Mealy BT", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
