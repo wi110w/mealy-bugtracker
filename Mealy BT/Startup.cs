2820446548c72c2bcb7616855d28dce0cd2a3d0a
Eugenia Glinska <wi110w@nyan.org.ua>
2021-07-26 21:20:18 +0300

    Add connection to DB

--- Mealy BT/Startup.cs
+++ Mealy BT/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Mealy_BT
@@ -27,6 +21,9 @@ namespace Mealy_BT
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEntityFrameworkNpgsql().AddDbContext<Mealy_BT.Models.MealyContext>
+            (opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("MealyConnection")));
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Mealy BT", Version = "v1"}); });
         }
 
