cf5cbfe21d4639447ed4d409ed8c7e4b40accf3f
Eugenia Glinska <wi110w@nyan.org.ua>
2021-07-26 21:20:34 +0300

    Clean controller

--- Mealy BT/Controllers/TaskController.cs
+++ Mealy BT/Controllers/TaskController.cs
@@ -11,29 +11,11 @@ namespace Mealy_BT.Controllers
     [Route("[controller]")]
     public class TaskController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<TaskController> _logger;
 
         public TaskController(ILogger<TaskController> logger)
         {
             _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
         }
     }
 }
