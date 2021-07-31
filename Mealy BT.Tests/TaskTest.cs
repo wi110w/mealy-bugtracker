606fb7496bbb02461c669e1ac856480ddea29cdd
Eugenia Glinska <wi110w@nyan.org.ua>
2021-07-31 15:37:16 +0300

    Add unit test skeleton

---
+++ Mealy BT.Tests/TaskTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace MealyBT.Tests
+{
+    public class TaskTest
+    {
+        [Fact]
+        public void TestCreation()
+        {
+            
+        }
+    }
+}
